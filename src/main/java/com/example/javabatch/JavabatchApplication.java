c4311da529741a978ddd5e802c09bb8d42705822
Vijender <vijender.kumar@arthmate.co.in>
2024-11-29 21:15:53 +0530

    Base Setup done for Java Batch

---
+++ src/main/java/com/example/javabatch/JavabatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javabatch/JavabatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
